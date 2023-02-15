c179a16c90bf900e318ba23dfae744dc03eec94e
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-02-15 10:38:47 -0500

    add create book logic

--- backend/books.py
+++ backend/books.py
@@ -22,12 +22,25 @@ BOOKS = [
     }
 ]
 
+# Read all books
 def read_all():
     return BOOKS
 
+# Read one book
 def read_one(id):
     book = [book for book in BOOKS if book['id'] == id]
     if book:
         return book[0]
     else:
         abort(404, f'Book with id {id} not found')
+
+# Create a book
+def create(book):
+    title = book.get('title')
+
+    if title and title not in [book['title'] for book in BOOKS]:
+        book['id'] = uuid.uuid4().hex
+        BOOKS.append(book)
+        return book, 201
+    else:
+        abort(409, f'Book with title {title} already exists')
