0b0f13f5000f3edfe75586a0a9c34bfe1fb5cf9f
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-02-15 11:31:07 -0500

    setup services to fetch data from api

---
+++ frontend/src/app/services/book.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.models';
+
+const API_URL = 'http://localhost:8000/api/books';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(API_URL);
+  }
+}
