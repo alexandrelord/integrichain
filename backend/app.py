03dba7c044a7cd8f570f4dbf763e0396daf4a204
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-02-15 10:29:34 -0500

    implement flask_cors

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
 import connexion
+from flask_cors import CORS
 
 app = connexion.App(__name__, specification_dir='./')
 app.add_api('swagger.yml')
-
-@app.route('/')
-def index():
-    return "Hello World!"
+CORS(app.app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
