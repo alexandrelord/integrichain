821a029136f20c290c177dcd919bc6bed2839ecf
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2023-02-15 11:30:26 -0500

    setup books component

---
+++ frontend/src/app/components/books/books.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../../models/book.models';
+import { BookService } from '../../services/book.service';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  books: Book[] = [];
+  
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.bookService.getBooks().subscribe({
+      next: books => {
+        this.books = books;
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    })
+  }
+
+  
+
+}
